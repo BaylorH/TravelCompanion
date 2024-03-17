2fb27c19e43b56ce186453de5a563b67ecce956d
Baylor Harrison <bp21harrison@gmail.com>
2024-03-16 20:26:03 -0700

    Initial Commit

---
+++ TravelCompanion/TravelCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelCompanionApp.swift
+//  TravelCompanion
+//
+//  Created by Baylor Harrison on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
